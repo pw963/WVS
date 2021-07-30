70cd7b254b9210f13a2c82c859e8f4fde411f7cb
pw963 <78516518+pw963@users.noreply.github.com>
2021-07-29 22:59:43 -0400

    Create globals.py

---
+++ config/globals.py
@@ -0,0 +1,9 @@
+extensions = [
+    "cogs.help",
+    "cogs.game_punishments.ban",
+    "cogs.game_punishments.kick",
+    "cogs.game_punishments.unban",
+    "cogs.game_punishments.warn",
+    "cogs.settings.setchannel",
+    "cogs.verification.verify"
+]
